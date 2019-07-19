91fd1df8b25609ce967c187f97065d5deaeca1a3
milad noori <miladnoori@berkeley.edu>
2019-07-19 13:44:51 -0500

    first push

---
+++ data_generator.py
@@ -0,0 +1,142 @@
+import os
+import numpy as np
+import cv2
+from tensorflow import keras
+from mask_img import LABEL_SORTED_TRAIN_ID
+
+
+def get_data(mode):
+    """
+    Function get_data(mode) accepts a string mode (train, val, test) and groups the paths of
+    the images from the img_folder together, while also doing the same task with the
+    images from the gtimg_folder. this function return 2 lists of these groups.
+
+    :param mode: (type : string): "train, "val", "test"
+    :return: a list of ids of images and masks
+    """
+    if mode == 'train' or mode == 'val' or mode == 'test':
+        img_folder_path = "citys/leftImg8bit"
+        gtimg_folder_path = "citys/gtFine"
+        x_paths = []
+        y_paths = []
+        tmp_img_folder_path = os.path.join(img_folder_path, mode)
+
+        # walk helps finding all files in a directory
+        # saving all the images in the img_folder
+        for (path, _, files) in os.walk(tmp_img_folder_path):
+            for file_name in files:
+                if file_name.endswith('.png'):
+                    x_paths.append(os.path.join(path, file_name))
+        # saving all the images in the gtimg_folder
+        idx = len(tmp_img_folder_path)
+        for x_path in x_paths:
+            y_paths.append(gtimg_folder_path + '/{}'.format(mode)+ x_path[idx:-15] + 'gtFine_color.png')
+        assert len(y_paths) == len(x_paths)
+        return x_paths, y_paths
+    else:
+        print("please choose the right mode (train, val, test)")
+
+
+class DataGen(keras.utils.Sequence):
+    """
+    this class is made for Data generation to map all the masks to the data we are going to use
+    you need to define a mode for the class ('train', 'val', 'test') to generate data
+    ex:
+    data_gen = DataGen(mode='train')
+    """
+    def __init__(self, mode, batch_size=8, image_width=1024, image_height=2048, image_depth=3, split=False, amount=1):
+        self.train_ids, self.mask_train_ids = get_data('train')
+        self.val_ids, self.mask_val_ids = get_data('val')
+        self.test_ids, self.mask_test_ids = get_data('test')
+        self.split = split
+        self.amount = amount
+        if self.split:
+            self.train_ids, self.mask_train_ids = self.train_ids[:amount], self.mask_train_ids[:amount]
+            self.val_ids, self.mask_val_ids = self.val_ids[:amount], self.mask_val_ids[amount]
+            self.test_ids, self.mask_test_ids = self.test_ids[:amount], self.mask_test_ids[:amount]
+        self.mode = mode
+        self.batch_size = batch_size
+        self.image_width = image_width
+        self.image_height = image_height
+        self.image_depth = image_depth
+        self.on_epoch_end()
+
+    def __load__(self, id_num):
+        """
+         A function to load the corresponding both the image and the mask
+
+        :param id_num: (type : int) the index of the ID in the list
+        :return: 2 np.array, one for image, and one for masks
+        """
+        mode = self.mode
+        if mode =='train':
+            image_path = self.train_ids[id_num]
+            mask_path = self.mask_train_ids[id_num]
+        elif mode == 'val':
+            image_path = self.val_ids_ids[id_num]
+            mask_path = self.mask_train_ids[id_num]
+        elif mode == 'test':
+            image_path = self.test_ids[id_num]
+            mask_path = self.mask_train_ids[id_num]
+        else:
+            print("mode does not exist")
+            return
+        image = cv2.imread(image_path, 1)
+        image = cv2.resize(image, (self.image_height, self.image_width))
+
+        image = image/255.0
+
+        mask_img = cv2.imread(mask_path, 1)
+        mask_img = cv2.cvtColor(mask_img, cv2.COLOR_BGR2RGB)
+        masking = np.zeros((1024, 2048, 21))
+        count = 0
+        for trian_id in LABEL_SORTED_TRAIN_ID:
+            lis = []
+            for label in trian_id:
+                usable_mask = mask_img
+                color_mask = np.asarray(label.color)
+                usable_mask = cv2.inRange(usable_mask, color_mask, color_mask)
+                lis.append(usable_mask)
+            label_mask = sum(lis)
+            masking[:, :, count] = label_mask
+            count += 1
+        mask = masking
+
+        return image, mask
+
+    def __getitem__(self, index):
+        dic = {
+            "train": self.train_ids,
+            "val": self.val_ids,
+            "test": self.test_ids
+        }
+        ids = dic[self.mode]
+        if (index+1)*self.batch_size > len(ids):
+            self.batch_size = len(ids) - index*self.batch_size
+        files_batch = ids[index*self.batch_size : (index+1)*self.batch_size]
+
+        image = np.zeros((self.batch_size, 1024, 2048, 3))
+        mask = np.zeros((self.batch_size, 1024, 2048, 21))
+        count = 0
+        for id_name in files_batch:
+            # print("getting in the loop : ", count)
+            _img, _mask = self.__load__(ids.index(id_name))
+            image[count] = _img
+            mask[count] = _mask
+            count += 1
+
+        return image, mask
+
+    def on_epoch_end(self):
+        pass
+
+    def __len__(self):
+        mode = self.mode
+        if mode =='train':
+            return int(np.ceil(len(self.train_ids)/float(self.batch_size)))
+        elif mode == 'val':
+            return int(np.ceil(len(self.val_ids)/float(self.batch_size)))
+        elif mode == 'test':
+            return int(np.ceil(len(self.test_ids)/float(self.batch_size)))
+        else:
+            print("mode does not exist")
---
+++ model.py
@@ -0,0 +1,170 @@
+# This python file implements the model for Fast-SCNN
+import os
+import tensorflow as tf
+from data_generator import DataGen
+
+
+def conv_layer(input_layer, conv_type, filters, kernel_size, strides, padding='same', relu=True, add_layer=None):
+    """
+    The function conv_Layer is an abstraction for making layers instead of
+    using tf.keras.layers.?? this function supports:
+        >> tf.keras.layers.Conv2D
+        >> tf.keras.layers.SeparableConv2D
+        >> tf.keras.layers.BatchNormalization
+        >> tf.keras.activations
+
+    :param input_layer: (type : tf.keras.layers.Input): the input to this layer
+    :param conv_type: (type : string): defining the type of this convolution ("conv", "ds", "dw", "add")
+    :param filters: (type : int): the number of filters/kernel
+    :param kernel_size: (type : tuple): the shape of the kernel
+    :param strides: (type : tuple): the stride for convolution
+    :param padding: (type : string): the the padding for image
+    :param relu: (type : boolean): true or false using relu activation
+    :param add_layer: (type : tf.keras.layers): to add 2 layers together with conv_type "add"
+    :return: (type : keras.layers.Layer)
+    """
+    if conv_type == 'ds':
+        x = tf.keras.layers.SeparableConv2D(filters, kernel_size, strides, padding=padding)(input_layer)
+    elif conv_type == 'dw':
+        x = ff_layer2 = tf.keras.layers.DepthwiseConv2D(filters, strides, depth_multiplier=1, padding=padding)(input_layer)
+    elif conv_type == 'add':
+        x = tf.keras.layers.add([input_layer, add_layer])
+    else:
+        x = tf.keras.layers.Conv2D(filters, kernel_size, strides, padding=padding)(input_layer)
+    x = tf.keras.layers.BatchNormalization()(x)
+    if relu:
+        x = tf.keras.activations.relu(x)
+    return x
+
+
+def bottelneck(input_layer, filters, kernel_size, expansion_factor, n, strides):
+    """
+    Bottleneck function this function takes care of each bottleneck in th e paper
+    each bottleneck consists of a Conv2D, DWConv, and Conv2D.
+    and repeated 'n' times
+
+    :param input_layer: (type : tf.keras.layers.Input): the input to this layer
+    :param filters: (type : int): the number of filters/kernel
+    :param kernel_size: (type : tuple): the shape of the kernel
+    :param expansion_factor: (type : int): this number is used in the inner function
+    :param n: (type : int): how many times to repeat the bottleneck
+    :param strides: (type : tuple): the stride for convolution
+    :return: x (type : keras.layers.Layer)
+    """
+    def _inner_bottelneck(input_layer, filters, kernel_size, expansion_factor, strides, add=False):
+        """
+        this function is used in the inner loop to continue adding the rest of the bottleneck
+
+        :param input_layer: (type : tf.keras.layers.Input): the input to this layer
+        :param filters: (type : int): the number of filters/kernel
+        :param kernel_size: (type : tuple): the shape of the kernel
+        :param expansion_factor: (type : int): expansion factor of the bottleneck block
+        :param strides: (type : tuple): the stride for convolution
+        :param add: (type : boolean): use for tf.keras.layers.add if set to True
+        :return: x (type : keras.layers.Layer)
+        """
+        tchannel = tf.keras.backend.int_shape(input_layer)[-1] * expansion_factor
+        x = conv_layer(input_layer, 'conv', tchannel, (1, 1), strides=(1, 1))
+        x = tf.keras.layers.DepthwiseConv2D(kernel_size, strides=strides, depth_multiplier=1, padding='same')(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+        x = tf.keras.activations.relu(x)
+        x = conv_layer(x, 'conv', filters, (1, 1), strides=(1, 1), padding='same', relu=False)
+        if add:
+            x = tf.keras.layers.add([x, input_layer])
+        return x
+    x = _inner_bottelneck(input_layer, filters, kernel_size, expansion_factor, strides)
+    for i in range(1, n):
+        x = _inner_bottelneck(x, filters, kernel_size, expansion_factor, (1, 1), True)
+    return x
+
+
+# NOTE i did not implement this i just copied it
+def ppm(input_tensor, bin_sizes):
+    """
+    Pyramid Pooling Block this function takes the feature map from the last convolution
+    layer and applies multiple sub-region average pooling and upscaling
+
+    :param input_tensor: (type : tensor) Feature Map
+    :param bin_sizes: (type : list) different bin sizes
+    :return:(type : keras.layers.Layer)
+    """
+    concat_list = [input_tensor]
+    w = 32
+    h = 64
+    for bin_size in bin_sizes:
+        x = tf.keras.layers.AveragePooling2D(pool_size=(w//bin_size, h//bin_size), strides=(w//bin_size, h//bin_size))(input_tensor)
+        x = tf.keras.layers.Conv2D(128, 3, 2, padding='same')(x)
+        x = tf.keras.layers.Lambda(lambda x: tf.image.resize(x, (w,h)))(x)
+        concat_list.append(x)
+    return tf.keras.layers.concatenate(concat_list)
+
+
+NO_OF_TRAINING_IMAGES = len(grouping('train')[0])
+NO_OF_VAL_IMAGES = len(grouping('val')[0])
+NO_OF_EPOCHS = 10
+BATCH_SIZE = 32
+
+
+
+# input image
+input_layer = tf.keras.layers.Input(shape=(1024, 2048, 3))
+# learning to down sample
+lds_layer = conv_layer(input_layer, 'conv', 32, (3, 3), (2, 2)) # size = (1024, 512, 32)
+lds_layer = conv_layer(lds_layer, 'ds', 48, (3, 3), (2, 2)) # size = (512, 256, 48)
+lds_layer = conv_layer(lds_layer, 'ds', 64, (3, 3), (2, 2)) # size = (256, 128, 64)
+# global feature extractor
+gfe_layer = bottelneck(lds_layer, 64, (3, 3), 6, 3, (2, 2)) # size = (128, 64, 128)
+gfe_layer = bottelneck(gfe_layer, 96, (3, 3), 6, 3, (2, 2)) # size = (128, 64, 128)
+gfe_layer = bottelneck(gfe_layer, 128, (3, 3), 6, 3, (1, 1)) # size = (128, 64, 128)
+ppm_layer = ppm(gfe_layer, [2,4,6,8])
+# Feature Fusion
+ff_layer1 = conv_layer(lds_layer, 'conv', 128, (1, 1), (1, 1), relu=False)  # size = (256, 128, 128)
+ff_layer2 = tf.keras.layers.UpSampling2D((4, 4))(ppm_layer)  # size = (256, 128, 128)
+ff_layer2 = conv_layer(ff_layer2, 'dw', 128, (1, 1), (1, 1))  # size = (256, 128, 128)
+ff_layer2 = tf.keras.layers.Conv2D(128, (1, 1), (1, 1), padding='same')(ff_layer2)  # size = (256, 128, 128)
+ffout_layer = conv_layer(ff_layer2, 'add', 0, (0, 0), (0, 0), add_layer=ff_layer1)  # adding the layers
+# # classifier
+classifier_layer = conv_layer(ffout_layer, 'ds', 128, (3, 3), (1, 1))  # size = (256, 128, 128)
+classifier_layer = conv_layer(classifier_layer, 'ds', 128, (3, 3), (1, 1))  # size = (256, 128, 128)
+classifier_layer = conv_layer(classifier_layer, 'conv', 21, (1, 1), (1, 1))  # size = (256, 128, 21)
+classifier_layer = tf.keras.layers.Dropout(0.3)(classifier_layer)
+classifier_layer = tf.keras.layers.UpSampling2D((8, 8))(classifier_layer)
+classifier_layer = tf.keras.activations.softmax(classifier_layer)
+
+
+
+
+
+fast_scnn_model = tf.keras.Model(inputs=input_layer, outputs=classifier_layer, name='Fast-SCNN')
+optimizer = tf.keras.optimizers.SGD(momentum=0.9, lr=0.045)
+fast_scnn_model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+fast_scnn_model.summary()
+
+
+# defining the training generator, validation generator and test generator with batch size 32
+
+# train_gen = data_gen('train', BATCH_SIZE, split=True, data=BATCH_SIZE)
+# val_gen = data_gen('val', BATCH_SIZE, split=True, data=BATCH_SIZE)
+# test_gen = data_gen('test', BATCH_SIZE, split=True, data=BATCH_SIZE)
+
+train_gen = DataGen('train', batch_size=BATCH_SIZE, split=True, amount=2*BATCH_SIZE)
+val_gen = DataGen('val', batch_size=BATCH_SIZE, split=True, amount=2*BATCH_SIZE)
+test_gen = DataGen('test', batch_size=BATCH_SIZE, split=True, amount=2*BATCH_SIZE)
+
+train_steps = len(train_gen)
+valid_steps = len(val_gen)
+
+
+# setting checkpoints
+checkpoint_path = os.path.join(os.getcwd(), "check/cp-{epoch:04d}.ckpt")
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    checkpoint_path, verbose=1, save_weights_only=True,
+    save_freq=5)
+fast_scnn_model.save_weights(checkpoint_path.format(epoch=0))
+
+# starting to train the model
+history = fast_scnn_model.fit_generator(train_gen, epochs=NO_OF_EPOCHS,
+                                        steps_per_epoch =train_steps,
+                                        validation_data=val_gen,
+                                        validation_steps=valid_steps,
+                                        callbacks=[cp_callback])
